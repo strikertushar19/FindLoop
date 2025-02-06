79bff8457ba7e84c71f5dfe7cffb5a00d376bedb
strikertushar19 <tushardogra19@gmail.com>
2025-02-06 11:30:48 +0530

    small changes

--- app/main.py
+++ app/main.py
@@ -1,17 +1,25 @@
 from fastapi import FastAPI, HTTPException, Query
 from utils import get_pdf_links, download_pdfs, extract_text_from_pdfs
 from summarizer import summarize_text
-
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+    
+)
 
 @app.get("/")
 def home():
     return {"message": "Welcome to the PDF Summarizer API!"}
 
-@app.get("/summarize/")
-def summarize(query: str = Query(..., description="Google search query"), num_pages: int = 1):
+@app.get("/Search/")
+def summarize(query: str = Query(..., description="Google search query")):
     try:
-        pdf_links = get_pdf_links(query, num_pages)
+        pdf_links = get_pdf_links(query)
         print(pdf_links)
         if not pdf_links:
             return {"error": "No PDFs found"}
